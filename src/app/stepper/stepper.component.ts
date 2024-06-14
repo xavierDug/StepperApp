61f52bbaa9363861322b7b8fbf5ad2f5de1c9769
xavier <xavierdugal2004@hotmail.com>
2024-06-13 21:57:00 -0400

    PROGRESS

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -8,22 +8,49 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class StepperComponent implements OnInit {
   // In your component.ts
-  steps = new Array(9).fill(null).map((_, index) => index + 1);
+  steps = new Array(10).fill(null).map((_, index) => index + 1);
   currentStep = 1; // This can be dynamically updated based on user interaction
 
-  formGroup!: FormGroup;
+  formGroup1!: FormGroup;
+  formGroup2!: FormGroup;
+
+  // this is the garage name
   inputText: string = '';
+
+  // this is the sticker template
+  inputSelect: string = '';
+
+  // this is the image url
+  selectedImageUrl: string | ArrayBuffer | null | undefined = null;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.formGroup = this.formBuilder.group({
+    this.formGroup2 = this.formBuilder.group({
       inputText: ['']
+    });
+    this.formGroup1 = this.formBuilder.group({
+      inputSelect: ['']
     });
   }
 
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.selectedImageUrl = e.target?.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
   updateText(): void {
-    this.inputText = this.formGroup.get('inputText')?.value;
+    this.inputText = this.formGroup2.get('inputText')?.value;
+  }
+
+  updateSelectTemplate(): void {
+    this.inputSelect = this.formGroup1.get('inputSelect')?.value;
   }
 
   nextStep() {

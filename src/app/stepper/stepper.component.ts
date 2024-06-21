ff3c0fdf013cbabdebba2c84568e26593a77b14f
xavier <xavierdugal2004@hotmail.com>
2024-06-20 22:06:12 -0400

    FCT text shrink

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -89,6 +89,19 @@ export class StepperComponent implements OnInit {
   ngOnInit() {
   }
 
+  calculateFontSize(text: string): number {
+    const baseSize = 24; // Starting font size in pixels for up to 14 characters
+    const maxLength = 15; // Start decreasing font size after this character count
+    const decreaseFactor = 0.5; // Decrease font size by 0.5px for each character beyond maxLength
+
+    if (text.length > maxLength) {
+      const decreaseAmount = (text.length - maxLength) * decreaseFactor;
+      return Math.max(baseSize - decreaseAmount, 10); // Ensure font size does not go below 10px
+    }
+
+    return baseSize; // Return base size if text length is within maxLength
+  }
+
   addInformationToArray() {
     const sticker = {
       selectedImageUrl: this.selectedImageUrl,
